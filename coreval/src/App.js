558877411890e443c8cea796bb91e61cf9c5cfed
Thoa Nguyen <thoalala@uw.edu>
2017-12-02 15:07:57 -0800

    Changed website layout

--- coreval/src/App.js
+++ coreval/src/App.js
@@ -9,11 +9,16 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="jumbotron-fluid bg-#243a51">
-          <h1 className="display-3 text-warning">CoreVal</h1>
+        <header className="jumbotron-fluid mt-4">
+          <img src={require('../src/storm.png')} className="img float-left" alt="logo"></img>
         </header>
-
       </div>
+
+
+
+
+
+
     );
   }
 }
