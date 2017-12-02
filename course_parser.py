a4f289c950a87b43b224e47b58feeeede217c2b6
Tuan Ma <tuanma123@gmail.com>
2017-12-02 13:29:21 -0800

    Implemented parsing the entire catalog for links for each course.

--- course_parser.py
+++ course_parser.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 
 
-def setup_browser():
+def setup_browser(headless):
     """
     This method sets up a browser for the scraper.
 
@@ -10,8 +10,10 @@ def setup_browser():
 
     options = webdriver.ChromeOptions()
     options.add_experimental_option('prefs', {'credentials_enable_service': False})  # Ignore saving password
+    if headless:
+        options.add_argument("headless")
     browser = webdriver.Chrome(executable_path="chromedriver.exe", chrome_options=options)  # Create the Chrome browser
-    browser.get("https://www.washington.edu/cec/a-toc.html")  # Go to Venmo URL to login.
+    browser.get("https://www.washington.edu/cec/a-toc.html")  # Go to UW URL to login.
 
     # Log in to UW.
     username_location = browser.find_element_by_id("weblogin_netid")
@@ -44,6 +46,13 @@ def parse_letter(letter, browser):
     return courses
 
 
+def parse_all_letters(browser):
+    all_links = []
+    for x in range(97, 123):
+        all_links.append(parse_letter(chr(x), browser))
+    print(all_links)
+
+
 def main():
     # Navigate theo global url for a letter.
     #base_url = "https://www.washington.edu/cec/" + letter + "-toc.html"
@@ -59,8 +68,7 @@ def main():
     #
     #
 
-    browser = setup_browser()
-    for x in range(97, 123):
-        # Append the character to the base URL.
-        parse_letter(chr(x), browser)
-parse_letter("a",setup_browser())
+    browser = setup_browser(True)
+    parse_all_letters(browser)
+
+main()
