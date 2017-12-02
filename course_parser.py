f3eb522204a16d82704108cc5e00449a3875cfc8
Tuan Ma <tuanma123@gmail.com>
2017-12-02 13:20:34 -0800

    Implemented the parsing for each letter.

--- course_parser.py
+++ course_parser.py
@@ -1,6 +1,3 @@
-from bs4 import BeautifulSoup
-import urllib.request
-
 from selenium import webdriver
 
 
@@ -14,12 +11,38 @@ def setup_browser():
     options = webdriver.ChromeOptions()
     options.add_experimental_option('prefs', {'credentials_enable_service': False})  # Ignore saving password
     browser = webdriver.Chrome(executable_path="chromedriver.exe", chrome_options=options)  # Create the Chrome browser
+    browser.get("https://www.washington.edu/cec/a-toc.html")  # Go to Venmo URL to login.
+
+    # Log in to UW.
+    username_location = browser.find_element_by_id("weblogin_netid")
+    username_location.send_keys("tuanma")
+
+    password_location = browser.find_element_by_id("weblogin_password")
+    password_location.send_keys("Lovelove1!")
+
+    send_button = browser.find_element_by_tag_name("input").submit()
     return browser
 
 
 def parse_letter(letter, browser):
-    browser.get("https://www.washington.edu/cec/c/CEE220A4696.html")
-    print(browser.page_source.split("\n"))
+    """
+    Parses the webpage for a given letter and returns a list of all course evaluations for that letter.
+
+    :param letter: The letter to query.
+    :param browser: The browser to use.
+    :return: A list containing the codes for all the course evaluations for the letter.
+    """
+    base_url = "https://www.washington.edu/cec/" + letter + "-toc.html"
+    browser.get(base_url)
+    html = browser.page_source.split("\n")[58:]
+    html = html[:html.index("<p></p>")]
+
+    courses = []
+    for line in html:
+        line = line[9:line.find(">") - 1]
+        courses.append(line)
+    return courses
+
 
 def main():
     # Navigate theo global url for a letter.
@@ -34,10 +57,10 @@ def main():
     # password_location = driver.find_element_by_id("weblogin_password")
     # password_location.send_keys("Lovelove1!")
     #
-    # send_button = driver.find_element_by_tag_name("input").submit()
+    #
 
     browser = setup_browser()
     for x in range(97, 123):
         # Append the character to the base URL.
         parse_letter(chr(x), browser)
-parse_letter("a")
+parse_letter("a",setup_browser())
