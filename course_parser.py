29cd7345c917d12c053caf4ada32f28ee7a156a5
William Kim <williamkim12345@hotmail.com>
2017-12-02 15:26:42 -0800

    Implemented course parser

--- course_parser.py
+++ course_parser.py
@@ -1,5 +1,5 @@
+import urllib.request
 from selenium import webdriver
-
 
 def setup_browser(headless):
     """
@@ -25,6 +25,9 @@ def setup_browser(headless):
     send_button = browser.find_element_by_tag_name("input").submit()
     return browser
 
+def parse_site(letter, browser):
+    browser.get("https://www.washington.edu/cec/" + letter)
+    return browser.page_source.split("\n")
 
 def parse_letter(letter, browser):
     """
@@ -45,30 +48,60 @@ def parse_letter(letter, browser):
         courses.append(line)
     return courses
 
-
 def parse_all_letters(browser):
     all_links = []
     for x in range(97, 123):
         all_links.append(parse_letter(chr(x), browser))
-    print(all_links)
+    return all_links
+
+def get_courseID(course):
+    finish = False
+    copy = course[2:]
+    result = ""
+    for x in copy:
+        if finish and x.isalpha():
+            return result
+
+        if x.isdigit():
+            finish = True
+        result = result + x
+
+def parse_course(course, html):
+    course_data = ["", "", "", "", ""]
+    course_data[1] = get_courseID(course)
+    ratings = []
+    for line in html:
+        if line.startswith("<tr><td>"):
+            ratings.append(line[len(line) - 14:-11])
+        elif line.startswith("<caption"):
+            for x in range(0, 3):
+                line = line[line.index("\"") + 1:]
+            line = line[:line.index("\"")]
+            course_data[4] = line
+        elif line.startswith("<h2>"):
+            line = line.replace(u'\xa0', u' ')
+            line = line[4:-5].split("   ")
+            course_data[0] = line[0]
+            course_data[3] = line[2]
+    course_data[2] = ratings
+    return course_data
 
 
 def main():
-    # Navigate theo global url for a letter.
-    #base_url = "https://www.washington.edu/cec/" + letter + "-toc.html"
-
-    # driver.get(base_url)  # Go to Venmo URL to login.
-    #
-    # # Log in to UW.
-    # username_location = driver.find_element_by_id("weblogin_netid")
-    # username_location.send_keys("tuanma")
-    #
-    # password_location = driver.find_element_by_id("weblogin_password")
-    # password_location.send_keys("Lovelove1!")
-    #
-    #
-
     browser = setup_browser(True)
-    parse_all_letters(browser)
+    all_courses = parse_all_letters(browser)
+    for letter_course in all_courses:
+        for course in letter_course:
+            html = parse_site(course, browser)
+            print(parse_course(course, html))
+            # adjust the print to include the professor, courseID, ratings (call above), quarter, number enrolled
 
 main()
+
+# prints all the numbers
+# print('[%s]' % ', '.join(map(str, extract_data(retrieve_lines(parse_letter("a/AA210A1311.html", setup_browser()))))))
+
+
+# place given file for specific professor
+# example --- parse_professor(the file from the site -> "a/AA210A1311")
+
