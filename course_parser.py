8f42a4e913dee738d415ab953f80bf5ee16813f8
Tuan Ma <tuanma123@gmail.com>
2017-12-02 17:21:52 -0800

    Cleaned up the formatting of the code for the parser. Added relevant comments for the methods.

--- course_parser.py
+++ course_parser.py
@@ -1,107 +1,208 @@
-import urllib.request
 from selenium import webdriver
+import csv
+import configparser
 
-def setup_browser(headless):
+
+def setup_browser(is_headless):
     """
-    This method sets up a browser for the scraper.
+    This methods sets up the browser object that is to be used for parsing.
 
-    :return: A Chrome browser object.
+    :param is_headless: This boolean value determines whether the browser is displayed as it is browsing.
+    :return: A Google Chrome browser object.
     """
 
+    # Setup your browser object and its options.
     options = webdriver.ChromeOptions()
-    options.add_experimental_option('prefs', {'credentials_enable_service': False})  # Ignore saving password
-    if headless:
+    options.add_experimental_option('prefs', {'credentials_enable_service': False})
+    # The parameter value determines whether or not the browser is visually shown.
+    if is_headless:
         options.add_argument("headless")
-    browser = webdriver.Chrome(executable_path="chromedriver.exe", chrome_options=options)  # Create the Chrome browser
-    browser.get("https://www.washington.edu/cec/a-toc.html")  # Go to UW URL to login.
+    browser = webdriver.Chrome(executable_path="chromedriver.exe", chrome_options=options)
 
-    # Log in to UW.
+    # Setup a parser for the config
+    config = configparser.ConfigParser()
+    username = config["credentials"]["username"]
+    password = config["credentials"]["password"]
+
+    # Navigate to a University of Washington page to login. Note that this username will be used to log in the entire
+    # time while scrapping.
+    browser.get("https://www.washington.edu/cec/a-toc.html")
+
+    # Input the username field.
     username_location = browser.find_element_by_id("weblogin_netid")
-    username_location.send_keys("tuanma")
+    username_location.send_keys(username)
 
+    # Input the password field.
     password_location = browser.find_element_by_id("weblogin_password")
-    password_location.send_keys("Lovelove1!")
+    password_location.send_keys(password)
 
+    # Send the information.
     send_button = browser.find_element_by_tag_name("input").submit()
     return browser
 
-def parse_site(letter, browser):
-    browser.get("https://www.washington.edu/cec/" + letter)
+
+def parse_site(course_link, browser):
+    """
+    Gets the raw HTML code for a given course.
+
+    :param course_link: The course code for the course.
+    :param browser: The browser object to use.
+    :return: A list containing the lines of the HTML code.
+    """
+
+    browser.get("https://www.washington.edu/cec/" + course_link)
     return browser.page_source.split("\n")
+
 
 def parse_letter(letter, browser):
     """
-    Parses the webpage for a given letter and returns a list of all course evaluations for that letter.
+    Parses the main directory for a given letter and returns a reference to all course that begin with said letter.
 
     :param letter: The letter to query.
     :param browser: The browser to use.
     :return: A list containing the codes for all the course evaluations for the letter.
     """
+    # Navigate to the URL.
     base_url = "https://www.washington.edu/cec/" + letter + "-toc.html"
     browser.get(base_url)
+
+    # Get the HTML code for the page. Remove any irrelevant lines.
     html = browser.page_source.split("\n")[58:]
+    # Cut off any lines that are needed.
     html = html[:html.index("<p></p>")]
 
+    # Stores all the URLs in a list and return them.
     courses = []
     for line in html:
         line = line[9:line.find(">") - 1]
         courses.append(line)
     return courses
 
+
 def parse_all_letters(browser):
+    """
+    Goes through the entire directory for a letter and then returns a list containing all the links for all letters.
+
+    :param browser: The browser object to use.
+    :return: A list of list, where each list is the course links for a specific letter.
+    """
     all_links = []
+    # Note that 'a' in ASCII is 97 and 'z' is 122, so we need to loop from 97 - 122 inclusive.
     for x in range(97, 123):
+        # chr(x) converts the integer to a character.
         all_links.append(parse_letter(chr(x), browser))
     return all_links
 
-def get_courseID(course):
+
+def get_course_id(course):
+    """
+    This method takes in a course code for a class and then extracts the course ID from it.
+
+    :param course: The course code to parse.
+    :return: The course ID for the class.
+    """
+
+    # This flag determines whether or not you have already seen a digit.
     finish = False
+
+    # Note that we do not want the section letter i.e. we want CSE 142 rather than CSE 142B. The flag remedies this.
+
+    # Get rid of the first two letters. We do not want the / and the first letter of the course.
     copy = course[2:]
+
+    # This holds the resulting string.
     result = ""
+
+    # Loop through every character and decide whether to add it.
     for x in copy:
+        # If you see have you already seen a digit and then you see a character, then you should stop.
         if finish and x.isalpha():
             return result
 
+        # If you see a digit, change the flag.
         if x.isdigit():
             finish = True
+
+        # If you have not already returned, add the character to the resulting string.
         result = result + x
 
+
 def parse_course(course, html):
+    """
+    Parses the URL of a class for the relevant data and puts them into a return value.
+
+    :param course: The course code.
+    :param html: The HTML code of the course page.
+    :return: A list containing all our data.
+    """
+    # Setup a list with dummy variables.
     course_data = ["", "", "", "", ""]
-    course_data[1] = get_courseID(course)
+    # Parse the course ID.
+    course_data[1] = get_course_id(course)
+    # Setup an array to hold all the ratings.
     ratings = []
+    # Go through every line in the HTML and check what kind of line it is. If its a special line then you should parse
+    # it for the relevant information.
     for line in html:
+        # This means it is a ratings line.
         if line.startswith("<tr><td>"):
+            # Delete any extra HTML code.
             ratings.append(line[len(line) - 14:-11])
+        # This means it is the line containing the number of students.
         elif line.startswith("<caption"):
+            # Delete all the text between the first 4 quotes.
             for x in range(0, 3):
                 line = line[line.index("\"") + 1:]
+            # Delete anything from the start to the first quote.
             line = line[:line.index("\"")]
+            # Add it to the list.
             course_data[4] = line
+        # This means it is the line that contains both the teacher name and the date that they taught.
         elif line.startswith("<h2>"):
+            # Replace the HTML hard spaces with normal spaces.
             line = line.replace(u'\xa0', u' ')
+            # Delete any extra HTML characters and split it.
             line = line[4:-5].split("   ")
+            # Do not add the second element in the line, it is the title of the professor. This is not really relevant
+            # to students so it is not required.
             course_data[0] = line[0]
             course_data[3] = line[2]
+    # Add the ratings after you have looped through everything.
     course_data[2] = ratings
     return course_data
 
 
-def main():
-    browser = setup_browser(True)
+def main(is_headless):
+    """
+    This is the main program that runs and connects all the methods together to parse the entire UW course catalog.
+
+    :param is_headless: A boolean value that is passed into the setting up browser to determine whether or not the
+    browser is graphically displayed when the parsing occurs.
+    :return: N/A void.
+    """
+    # Setup the browser.
+    browser = setup_browser(is_headless)
+    # Open a csv file to write and a csv writer.
+    csv_file = open("data.csv", "w")
+    csv_writer = csv.writer(csv_file)
+    # Gets all the courses.
     all_courses = parse_all_letters(browser)
+
+    # Loop through every list in the list of all courses.
     for letter_course in all_courses:
+        # Go through every course for each letter.
         for course in letter_course:
+            # Get the HTML code for the course.
             html = parse_site(course, browser)
-            print(parse_course(course, html))
+            # Parse the HTML code for the data needed.
+            course_data = parse_course(course, html)
+            # Replace the list in the 3rd argument with a string and replace the commas with spaces for easier parsing.
+            course_data[2] = str(course_data[2]).replace(", ", " ")
+
+            # Write the list to disk as the csv file.
+            csv_writer.writerow(course_data)
             # adjust the print to include the professor, courseID, ratings (call above), quarter, number enrolled
-
-main()
-
-# prints all the numbers
-# print('[%s]' % ', '.join(map(str, extract_data(retrieve_lines(parse_letter("a/AA210A1311.html", setup_browser()))))))
+    csv_file.close()
 
 
-# place given file for specific professor
-# example --- parse_professor(the file from the site -> "a/AA210A1311")
-
+main(True)
