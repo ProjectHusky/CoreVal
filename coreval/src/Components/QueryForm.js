e1b38ffa1269ae9f2f79f99bd254e1481505233d
Tony Vo <tonyvo@cs.washington.edu>
2017-12-04 03:43:49 -0800

    Enhancing the UI. Still need to make responsive and implement query

--- coreval/src/Components/QueryForm.js
+++ coreval/src/Components/QueryForm.js
@@ -7,58 +7,77 @@ export default class QueryFormView extends React.Component {
         super(props);
         this.state = {
             professor : "",
+            professorQueryFormat: "",
             classAbrev: "",
             courseArr: []
-
         }
     }
 
     // Send request to firebase
     handleSubmit(evt) {
-        evt.preventDefault();;
-        let db = firebase.database();
-        let path = "";
-        // Fields are empty.
-        if (!this.state.professor && !this.state.classAbrev) {
-            alert("Empty fields");
-        } else if (this.state.professor && !this.state.classAbrev) {
-            console.log("This one");
-            path = `/Professors/${this.state.professor}`
-        } else  if (this.state.classAbrev && !this.state.professor) {
-            path = `/Classes/${this.state.professor}`
-        } else {
-            path = `/Classes/${this.state.classAbrev}/${this.state.classAbrev}`
-        }
-        
-        let professor = this.state.professor;
-        let dataTemp = this.state.courseArr.slice();
-        db.ref(path).orderByValue().on("value", data => {
-            data.forEach(function(data) {
-                dataTemp.push(data.val());
-            });
-            this.setState({
-                courseArr: dataTemp
-            })
-        });
+        evt.preventDefault();
+        if (this.state.courseArr.length === 0) {
+            
+            let db = firebase.database();
+            let path = "";
 
+            let nameTokens = this.state.professor.toLowerCase().trim().split(' ');
+            let capTokens = nameTokens.map(token => token.charAt(0).toUpperCase() + token.slice(1));
+            let professorQueryFormat = capTokens.join('_');
+            console.log(professorQueryFormat);
+            if (!professorQueryFormat && !this.state.classAbrev) {
+                alert("Empty Fields");
+                return;
+            } else if (professorQueryFormat && !this.state.classAbrev) {
+                path = `/Professors/${professorQueryFormat}`
+            } else  if (this.state.classAbrev && !professorQueryFormat) {
+                path = `/Classes/${professorQueryFormat}`
+            } else {
+                path = `/Classes/${this.state.classAbrev}/${this.state.classAbrev}`
+            }
+            let queryResultTemp = this.state.courseArr.slice();
+            // Query on the path and get a snapshot of the results.
+            db.ref(path).orderByValue().on("value", querySnapshot => {
+                // Add each query result to the temp array and update
+                // and update the state.
+                querySnapshot.forEach(function(queryResultObj) {
+                    let queryResult = queryResultObj.val();
+                    queryResultTemp.push(queryResult);
+                });
+                this.setState({
+                    courseArr: queryResultTemp
+                });
+            });
+        }
     }
 
     render() {
         return (
-            <div> 
-                <form onSubmit={evt => this.handleSubmit(evt)}>
+            <div className="pt-3 pb-3"> 
+                <form onSubmit={evt => this.handleSubmit(evt)} className="input-group">
                     <input type="text"
+                        id="searchbar"
                         className="form-control"
                         value={this.state.professor}
                         placeholder="Enter your professor"
+                        // Update the professor when the user types
                         onChange={evt => {
                             this.setState({
-                                professor: evt.target.value
+                                professor: evt.target.value,
+                                professorQueryFormat: "",
+                                classAbrev: "",
+                                courseArr: []
                             }) 
                         }}
                     />
+                    <span className="input-group-btn">
+                        <button id="searchbutton" type="button" className="btn btn-success">
+                            <span className="glyphicon glyphicon-search"> Search </span>
+                        </button>
+                    </span>
                 </form>
-                <QueryListView result={this.state.courseArr} professor={this.state.professor}/>
+                {/* Create a QueryListView which renders a bunch of Query Results */}
+                <QueryListView results={this.state.courseArr} prof={this.state.professor}/>
             </div>
         );
     }
--- coreval/src/Components/QueryResult.js
+++ coreval/src/Components/QueryResult.js
@@ -4,11 +4,37 @@ import "firebase/database";
 export default class QuerySearchView extends React.Component {
     render() {
         let info = this.props.courseInfo;
+        console.log(info);
+        let rating = info.rating;
+        for (var category in rating) {
+            console.log(category);
+            console.log(typeof rating[category]);
+            rating[category] = parseFloat(rating[category].replace("'",""));
+        }
         return (
-            <div>
-                <p> {this.props.professor} </p>
-                <p>{info.rating} {info.quarter} </p>
-            </div>
+            <tr>
+                <td>{info.course}</td>
+                <td>{info.quarter}</td>
+                <td>{info.numSurveyed}</td>
+                <td>
+                    <table className="table table-sm table-condensed">
+                        <thead>
+                            <td>Overall</td>
+                            <td>Content</td>
+                            <td>Contribution</td>
+                            <td>Effectiveness</td>
+                        </thead>
+                        <tbody>
+                            <td>{rating.overall}</td>
+                            <td>{rating.content}</td>
+                            <td>{rating.contribution}</td>
+                            <td className="ratingTable">{rating.effective}</td>
+                        </tbody>
+                    </table>
+                </td>
+                    
+                    {/* {info.rating.overall}</td> */}
+            </tr>
         );
     }
 }
