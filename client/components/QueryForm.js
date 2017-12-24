b3af38d9e0c90a3c168dfe0d2b7cb576579f9cb6
Tony Vo <tonyt.vo22@gmail.com>
2017-12-24 04:03:10 -0800

    Working on front end queries

--- client/components/QueryForm.js
+++ client/components/QueryForm.js
@@ -1,63 +1,75 @@
 import React from "react";
 import QueryListView from "./QueryList"
+
 export default class QueryFormView extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            query: "",
-            resultArr: [],
+            activeQuery: "",    // Keep track of what the user type
+            currentQuery: "",   // Keep track of what the user last queried.
+            resultArr: [],      // Keep track of the current results
             searchFinished:  false
         }
     }
 
-
-    // Send request to firebase
+    // Send request to our our api upon submit.
     handleSubmit(evt) {
+        console.log(evt);
         evt.preventDefault();
-        fetch(`/api/eval/${this.state.query}`, {
-        }).then((response) => {
-            console.log(response);
-            return response.json();
-        }).then((jsonResponse) => {
-            this.setState({
-                resultArr: jsonResponse.results,
-                searchFinished: true
-            })
-            console.log(jsonResponse.results);
-        }).catch((err) => {
-            console.log(err);
-        });
+        if (this.state.activeQuery.length >= 3) {
+            fetch(`/api/eval/${this.state.activeQuery}`, {
+            }).then((response) => {
+                return response.json();
+            }).then((jsonResponse) => {
+                this.setState({
+                    currentQuery: this.state.activeQuery,
+                    resultArr: jsonResponse.results,
+                    searchFinished: true
+                })
+                console.log(jsonResponse.results);
+            }).catch((err) => {
+                console.log(err);
+            });
+        }
     }
 
     render() {
-        console.log(this.state.resultArr);
         return (
-            <div className="pt-3 pb-3"> 
-                <form onSubmit={evt => this.handleSubmit(evt)} className="input-group">
-                    <input type="text"
-                        id="searchbar"
-                        className="form-control"
-                        value={this.state.query}
-                        placeholder="Enter your professor or class"
-                        // Update the professor when the user types
-                        onChange={evt => {
-                            this.setState({
-                                query: evt.target.value,
-                                searchFinished: false
-                            }) 
-                        }}
-                    />
-                    <span className="input-group-btn">
-                        <button id="searchbutton" type="button" className="btn btn-success">
-                            <span className="glyphicon glyphicon-search"> Search </span>
-                        </button>
-                    </span>
-                </form>
-                
-                {/* Create a QueryListView which renders a bunch of Query Results */}
-                {this.state.searchFinished 
-                    ? <QueryListView results={this.state.resultArr}/> 
-                    : "Welcome" }
+            <div className="container mt-1"> 
+                <div className="jumbotron">
+                    <div>
+                        <h1>CoreVal: A better way to browse evaluations </h1>
+                    </div>
+                </div>
+                <div className="jumbotron">
+                    <form onSubmit={evt => this.handleSubmit(evt)} className="input-group">
+                        <input type="text"
+                            id="searchbar"
+                            className="form-control"
+                            value={this.state.activeQuery}
+                            placeholder="Enter a UW Professor or Class"
+                            onChange={evt => {
+                                this.setState({
+                                    // Update the professor when the user types
+                                    activeQuery: evt.target.value,
+                                    searchFinished: false
+                                }) 
+                            }}
+                        />
+                        <span className="input-group-btn">
+                            <button id="searchbutton" type="button" className="btn btn-success">
+                                <span className="glyphicon glyphicon-search"> Search </span>
+                            </button>
+                        </span>
+                    </form>
+                    {/* Create a QueryListView which renders Query Results */}
+                    {this.state.searchFinished 
+                        ? <QueryListView query={this.state.currentQuery} results={this.state.resultArr}/> 
+                        : "Welcome" }
+                </div>
+                <div className="footer">
+                    CoreVal
+                </div>
             </div>
         );
     }
--- client/components/QueryResult.js
+++ client/components/QueryResult.js
@@ -1,30 +1,83 @@
 import React from "react";
 
 export default class QuerySearchView extends React.Component {
+
+    styleRating(rating) {
+        let ratingPerc = (rating / 5.0) * 100.0;
+        let backgroundColor = "";
+        if (rating <= 2) {
+            backgroundColor = "#CC0000";
+        } else if (rating <= 3) {
+            backgroundColor = "#FF8800";
+        } else if (rating <= 4) {
+            backgroundColor = "#ffbb33"; 
+        } else {
+            backgroundColor = "#00C851";
+        }
+        let styles = {
+            width: ratingPerc + "%",
+            backgroundColor
+        };
+
+        return {
+            rating: rating.toString(),
+            ratingPerc,
+            styles
+        }
+    }
     render() {
         let info = this.props.courseEval;
+        let overallRating = this.styleRating(info.overall);
+        let contentRating = this.styleRating(info.content);
+        let contributionRating = this.styleRating(info.contribution);
+        let effectivenessRating = this.styleRating(info.effectiveness);
         return (
-            <tr>
-                <td>{info.course}</td>
-                <td>{info.quarter}</td>
-                <td>{info.numSurveyed}</td>
-                <td>
-                    <table className="table table-sm table-condensed">
-                        <thead>
-                            <td>Overall</td>
-                            <td>Content</td>
-                            <td>Contribution</td>
-                            <td>Effectiveness</td>
-                        </thead>
-                        <tbody>
-                            <td>{info.overall}</td>
-                            <td>{info.content}</td>
-                            <td>{info.contribution}</td>
-                            <td className="ratingTable">{info.effectiveness}</td>
-                        </tbody>
-                    </table>
-                </td>
-            </tr>
+            <div className="card mt-4">
+                <div className="card-block">
+                    <h4 className="card-title">{info.course} {info.quarter}</h4>
+                    <h6 className="card-subtitle mb-2">{info.professor}</h6>
+                    <div className="row card-text">
+                        <div className="col-md-3">
+                            <p> Overall </p>
+                            <div className="progress">
+                                <div className="progress-bar" role="progressbar" aria-valuenow={overallRating.rating} aria-valuemin="0" aria-valuemax="5" style={overallRating.styles}>
+                                    <span className="sr-only">40% Complete (success)</span>
+                                    {overallRating.rating}
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-3">
+                            <p> Content </p>
+                            <div className="progress">
+                                <div className="progress-bar" role="progressbar" aria-valuenow={contentRating.rating} aria-valuemin="0" aria-valuemax="5" style={contentRating.styles}>
+                                    <span className="sr-only">40% Complete (success)</span>
+                                    {contentRating.rating}
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-3">
+                            <p> Contribution </p>
+                            <div className="progress">
+                                <div className="progress-bar" role="progressbar" aria-valuenow={contributionRating.rating} aria-valuemin="0" aria-valuemax="5" style={contributionRating.styles}>
+                                    <span className="sr-only">40% Complete (success)</span>
+                                    {contributionRating.rating}
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-3">
+                            <p> Effectivenes </p>
+                            <div className="progress">
+                                <div className="progress-bar" role="progressbar" aria-valuenow={effectivenessRating.rating} aria-valuemin="0" aria-valuemax="5" style={effectivenessRating.styles}>
+                                    <span className="sr-only">40% Complete (success)</span>
+                                    {effectivenessRating.rating}
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <a href="#" className="card-link">RateMyProfessor</a>
+                    <a href="#" className="card-link">Eval Link</a>
+                </div>
+            </div>
         );
     }
 }
