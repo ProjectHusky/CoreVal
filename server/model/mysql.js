4f8fe1aad88312a7ff4da1e1697f266a8da7d1d4
Tony Vo <tonyt.vo22@gmail.com>
2017-12-28 03:25:48 -0800

    Lowercase l...

--- server/model/mysql.js
+++ server/model/mysql.js
@@ -10,12 +10,13 @@
  */
 
 let mysql = require('mysql');
+
 // Establish a connection to databse.
 let connection = mysql.createConnection({
-    host: process.env.ClEARDB_DATABASE_HOST,
-    user: process.env.ClEARDB_DATABASE_USER,
-    password: process.env.ClEARDB_DATABASE_PASSWORD,
-    database: process.env.ClEARDB_DATABASE_DB
+    host: process.env.CLEARDB_DATABASE_HOST,
+    user: process.env.CLEARDB_DATABASE_USER,
+    password: process.env.CLEARDB_DATABASE_PASSWORD,
+    database: process.env.CLEARDB_DATABASE_DB
 });
 
 // Connect to database!
